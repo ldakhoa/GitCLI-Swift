5abb94798e0bb87661d410a5d7d042654c3f926f
ldakhoa <ldakhoa1308@gmail.com>
2022-11-19 01:49:38 +0700

    Remove unused code

--- Sources/GitSwiftCLI/GitSwiftCLI.swift
+++ Sources/GitSwiftCLI/GitSwiftCLI.swift
@@ -1,17 +1,6 @@
 import ArgumentParser
 import GitSwiftCore
 
-//@main
-//public struct GitSwiftCLI: ParsableCommand {
-//    public static let configuration = CommandConfiguration(
-//        commandName: "gitswift",
-//        abstract: "A Swift CLI tool to make your Git flow easier",
-//        version: "0.0.1"
-//    )
-//
-//    public init() {}
-//}
-
 struct Repo: ParsableCommand {
     public static let configuration = CommandConfiguration(
         abstract: "Work with GitHub repositories",
